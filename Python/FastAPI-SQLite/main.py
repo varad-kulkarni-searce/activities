2d4575c5c72076c0baa924307bb1640531be99a8
varad-kulkarni-searce <varad.kulkarni@searce.com>
2022-03-29 15:59:35 +0530

    updated

--- Python/FastAPI-SQLite/main.py
+++ Python/FastAPI-SQLite/main.py
@@ -1,3 +1,5 @@
+from urllib import request
+
 from fastapi import FastAPI
 import uvicorn
 from database import db
@@ -5,6 +7,7 @@ from database import db
 app = FastAPI()
 
 
+# get student details by id
 @app.api_route('/student_access', methods=["GET"])
 def student_db(student_id):
     return db.select(student_id)
@@ -16,26 +19,29 @@ def student_db(student_id):
 #                     ]
 #     return student_list
 
-
+# bulkWrite
 @app.api_route('/student_add_multiple', methods=["POST"])
 def add_multiple_students(student_id_1, student_name_1, student_class_1, student_marks_1, student_id_2, student_name_2,
                           student_class_2, student_marks_2):
     student_list = [(student_id_1, student_name_1, student_class_1, student_marks_1),
                     (student_id_2, student_name_2, student_class_2, student_marks_2),
                     ]
-    return db.multipleadd(student_list)
+    return db.bulkWrite(student_list)
 
 
+# Add student
 @app.api_route('/student_access', methods=['POST'])
 def add_student(student_id, student_name, student_class, student_marks):
     return db.write(student_id, student_name, student_class, student_marks)
 
 
+# Delete student
 @app.api_route('/student_access', methods=['DELETE'])
 def delete_student(student_id):
     return db.delete(student_id)
 
 
+# Read all students
 @app.api_route('/student_access_all', methods=["GET"])
 def all_student_db():
     return db.selectAll()
